32e2b5f7a5b6980d0f9eeb4a9966ee646a7e3987
peter <petr-niz1@yandex.ru>
2020-01-13 17:59:21 +0500

    ///

---
+++ Coffers.Public.WebApi.V2/src/Coffers.Public.Queries.Infrastructure/User/Entity/GamerView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Coffers.Public.Queries.Infrastructure.User.Entity
+{
+    internal sealed class GamerView
+    {
+        public static String Sql = @"SELECT 
+  u.Id,
+  u.Rank,
+  u.Status,
+  u.DateOfBirth,
+  u.Name,
+  SUM(uo.Amount) AS Balance
+FROM `User` u
+LEFT JOIN `Operation` uo ON uo.UserId = u.Id AND uo.Type IN ('Emission', 'Other')
+WHERE 1 = 1
+  AND u.GuildId = @GuildId
+  AND (u.DeleteDate IS NULL OR u.DeleteDate < @DeleteDate)
+  AND o.CreateDate < ADDDATE(@DeleteDate, MONTH 1)";
+    }
+}
