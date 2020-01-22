f6877964b90fb26eef34fd86436f06363f08028c
peter <petr-niz1@yandex.ru>
2020-01-22 18:50:57 +0500

    ///

---
+++ Coffers.Public.WebApi.V2/src/Coffers.Public.Domain/Penalties/Penalty.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Coffers.Types.Gamer;
+
+namespace Coffers.Public.Domain.Penalties
+{
+    public sealed class Penalty
+    {
+        /// <summary>
+        /// Идентификатор штрафа
+        /// </summary>
+        public Guid Id { get; }
+
+        public Guid UserId { get; }
+
+        /// <summary>
+        /// Сумма штрафа
+        /// </summary>
+        public Decimal Amount { get; }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime CreateDate { get; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Дата обновления
+        /// </summary>
+        public DateTime UpdateDate { get; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Статус штрафа
+        /// </summary>
+        public PenaltyStatus PenaltyStatus { get; } = PenaltyStatus.Active;
+
+        /// <summary>
+        /// Причина
+        /// </summary>
+        public String Description { get; }
+
+        public Guid ConcurrencyTokens { get; internal set; } = Guid.NewGuid();
+        public Penalty(Guid id, Guid userId, Decimal amount, String description)
+        => (Id, UserId, Amount, Description)
+        = (id, userId, amount < 0 ? throw new ArgumentOutOfRangeException(nameof(amount), "Non-negative number required") : amount, description?.Trim());
+    }
+}
