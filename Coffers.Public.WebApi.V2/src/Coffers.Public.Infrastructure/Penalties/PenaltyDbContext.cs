6d6781e5dfb58dc4844fec0820fb76ec7c8b1665
peter <petr-niz1@yandex.ru>
2020-01-23 19:35:13 +0500

    Add context

---
+++ Coffers.Public.WebApi.V2/src/Coffers.Public.Infrastructure/Penalties/PenaltyDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Coffers.Public.Domain.Penalties;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffers.Public.Infrastructure.Penalties
+{
+
+    public class PenaltyDbContext : DbContext
+    {
+        public DbSet<Penalty> Penalties { get; set; }
+
+        public PenaltyDbContext(DbContextOptions<PenaltyDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Penalty>(b =>
+            {
+                b.ToTable(nameof(Penalty));
+
+                b.HasIndex(p => p.Id)
+                    .IsUnique();
+                b.HasKey(p => p.Id);
+                b.Property(p => p.Id)
+                    .HasColumnName("Id")
+                    .IsRequired();
+
+                b.Property(p => p.CreateDate)
+                    .IsRequired();
+
+                b.Property(o => o.Amount)
+                    .HasDefaultValue(0)
+                    .IsRequired();
+
+                b.Property(p => p.Description)
+                    .HasMaxLength(2048);
+
+                b.Property(p => p.PenaltyStatus)
+                    .HasConversion<String>()
+                    .HasMaxLength(32);
+
+                b.Property(l => l.ConcurrencyTokens)
+                    .IsRequired()
+                    .IsConcurrencyToken();
+
+            });
+
+        }
+    }
+}
---
+++ Coffers.Public.WebApi.V2/src/Coffers.Public.Infrastructure/Penalties/PenaltyRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Coffers.Public.Domain.Penalties;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffers.Public.Infrastructure.Penalties
+{
+    public class PenaltyRepository : IPenaltyRepository
+    {
+        private readonly PenaltyDbContext _context;
+
+        public PenaltyRepository(PenaltyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Penalty> Get(Guid id, CancellationToken cancellationToken)
+        {
+            return await _context.Penalties
+                .FirstOrDefaultAsync(_ => _.Id == id, cancellationToken);
+        }
+
+        public async Task<Penalty> Get(Guid id, Guid userId, CancellationToken cancellationToken)
+        {
+            return await _context.Penalties
+                .FirstOrDefaultAsync(_ =>
+                    _.Id == id &&
+                    _.UserId == userId, cancellationToken);
+        }
+
+        public async Task Save(Penalty penalty)
+        {
+            var entry = _context.Entry(penalty);
+            if (entry.State == EntityState.Detached)
+                _context.Penalties.Add(penalty);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
