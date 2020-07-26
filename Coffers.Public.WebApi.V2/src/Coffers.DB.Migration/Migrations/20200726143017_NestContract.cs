26d6a6c1d46a2a61f1fa2605e31d8576da13afb2
peter <petr-niz1@yandex.ru>
2020-07-26 19:31:42 +0500

    добавил пропущенную миграцию

---
+++ Coffers.Public.WebApi.V2/src/Coffers.DB.Migration/Migrations/20200726143017_NestContract.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Coffers.DB.Migrations.Migrations
+{
+    public partial class NestContract : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reward",
+                table: "NestContract",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "NestContract",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reward",
+                table: "NestContract");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "NestContract");
+        }
+    }
+}
